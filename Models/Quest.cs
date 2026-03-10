44ebc2a0b6c5d62babd45b4db752f8e476f84601
moroza27 <moroz.a_ipz23@rcit.ukr.education>
2026-03-11 00:23:49 +0200

    Update layout, pages and models

---
+++ Models/Quest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SweetBakeryQuest.Models
+{
+    public class Quest
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Title { get; set; } = string.Empty;
+        public QuestDifficulty Difficulty { get; set; }
+        public int RewardGold { get; set; }
+        public int RewardReputation { get; set; }
+
+        public Dictionary<ProductType, int> RequiredIngredients { get; set; } = new();
+
+        public bool IsCompleted { get; set; } = false;
+    }
+}
