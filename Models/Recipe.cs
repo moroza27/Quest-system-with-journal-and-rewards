f07243e278162bb75372cc550a37ae6540c52835
moroza27 <moroz.a_ipz23@rcit.ukr.education>
2026-03-11 12:18:04 +0200

    Add Recipe Book UI to inventory

---
+++ Models/Recipe.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace SweetBakeryQuest.Models
+{
+    public class Recipe
+    {
+        public string Name { get; set; } = "";
+        public string Icon { get; set; } = "";
+        public QuestDifficulty Difficulty { get; set; }
+
+        public Dictionary<ProductType, int> Ingredients { get; set; } = new();
+    }
+}
